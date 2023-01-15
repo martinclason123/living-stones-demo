e706bfc88ade168d36b291517af5c9c175c712b9
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-01-14 20:00:40 -0500

    "initial commit"

---
+++ _next/static/dlmvO_zrw9rc79ztFwQCX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/95b64a6e-9562e008bb67a5b8.js","static/chunks/865-1dc985c5b0ac93a2.js","static/chunks/pages/index-0adf84624d969798.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
